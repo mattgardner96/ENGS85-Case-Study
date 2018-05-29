4c41734e6b2dc543afd05182e00471b3b87134a9
Matt Gardner <mattgardner96@gmail.com>
2018-05-29 13:43:00 -0400

    Current draw/power demand changes

--- Case_Study.m
+++ Case_Study.m
@@ -14,13 +14,13 @@
 
 clear all;
 
-max_motor_speed = 3000; %RPM
+max_rated_RPM = 3000; %RPM
 
 wheel_radius = 8; % 5" wheel radius and tires estimated to be 3" sidewall
 
 max_ground_speed = 25; %mph
 
-gear_ratio = max_motor_speed * wheel_radius * 2*pi * 60 * (1/(5280*12)) / max_ground_speed %#ok<NOPTS>
+gear_ratio = max_rated_RPM * wheel_radius * 2*pi * 60 * (1/(5280*12)) / max_ground_speed %#ok<NOPTS>
 %% 
 % Next, we calculated k & R for our chosen motor using given motor
 % parameter graphs.
@@ -31,7 +31,7 @@ torque_max = 4.05; % N-m
 torque_min = 0;
 current_max = 33.6; % A
 current_min = 1.6;
-RPM_max = 3000; %RPM
+RPM_max = 3700; %RPM
 RPM_min = 2873;
 k_motor = (torque_max - torque_min) / (current_max-current_min) %#ok
 
@@ -69,17 +69,24 @@ Cap_battery_FOS=Cap_battery*3 %#ok
 % total)
 % 
 % Does battery fulfill course requirements (6 2-second accelerations for 20
-% laps with constant 25mph speed otherwise, 1 min/lap)
+% laps with constant 25mph speed otherwise, 1 min/lap)? Yes...
 
 power_max = torque_max * 2*pi/60 * RPM_min;
 
-power_constant_speed = F_road * 10 * 0.0254 / gear_ratio * (2*pi/60) * RPM_max;
+power_constant_speed = F_road * wheel_radius * 0.0254 / gear_ratio * (2*pi/60) * RPM_max;
 
 % Accelerations & Constant Speed
 energy_reqd = (power_max * 2 * 6 + power_constant_speed * 60) * 20 * 0.0002777777778 %#ok
 
-%Accelerations
-power_reqd = power_max * 2 * 6 * 20 * 0.0002777777778;
+%%
+% Average power demand is calculated based on a constant 25 mph speed,
+% though the "energy_reqd" variable is based on the course requirements
+% (including the 2-sec accelerations). Current draw is calculated using the
+% equation $\tau = ki$.
+
+power_constant_speed %#ok
+
+current_draw_constant = F_road * wheel_radius * 0.0254 / gear_ratio / k_motor %#ok
 
 %Maximum Speed on 5 percent grade
 
@@ -89,5 +96,10 @@ grade_speed_no_drag = (v_motor - ((F_roll+grade_load)*wheel_radius*0.0254/gear_r
 % a =1/2 * 1.225 * C_d * A_front;
 % c = F_roll + grade_load;
 % Speed_max = power_max/(c+a*speed_max+b*speed_max^2);
-% 
-% 
+
+%% 
+% By solving the cubic equation $P = Fv = (av^2 + bv + c)v$, we were able
+% to calculate a *max speed of 15.7 mph at a 5% grade*. *Power demand* on the
+% hill would be the full power of the motor in the short term, *1218.81W*.
+% *Current draw would be 33.6A* at this power draw.
+
